5d9b0f640f49fba3f1a9451758f3a1a163f45fe4
kuroda600 <122330065+kuroda600@users.noreply.github.com>
2023-01-16 16:37:46 +0900

    Initial commit

---
+++ PHPjob/HelloWorld/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$num = 1; 
+while($num <= 100) {
+    if($num % 3 === 0 && $num % 5 === 0) {
+        echo 'FizzBuzz!';
+    } else if($num % 3 === 0) {
+        echo 'Fizz!';
+    } else if($num % 5 === 0) {
+        echo 'Buzz!';
+    } else {
+        echo $num;
+    }
+    $num++;
+    echo '<br>';
+}
+
+?>
