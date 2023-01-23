2dc6e1d27e155f59dd91d86c90988ac2ee73129a
kuroda600 <122330065+kuroda600@users.noreply.github.com>
2023-01-23 19:07:39 +0900

    練習問題２
    練習問題２の作成

--- PHPjob/3-3/index.php
+++ PHPjob/3-3/index.php
@@ -1,4 +1,4 @@
+<p>0～9の番号を使って好きな数字の羅列を入力してください。</p>
 <form action="result.php" method="post">
-    <p>0～9の番号を使って好きな数字の羅列を入力してください。</p>
     <input type="text" pattern="^[0-9]+$" name="number" /> <input type="submit" value="占う" />
 </form>
